ceab95db6408a67ded50303055cd5e0c1f78927f
kenlok <ken.lok@eezee.com.sg>
2022-03-09 16:54:24 +0800

    (feat): create BreadCrumbs component add to pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import GovBotProvider from 'components/context-provider/provider'
 import theme from 'config/theme'
 import NavBar from 'components/NavBar'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <ChakraProvider theme={theme}>
-        <NavBar />
-        <Component {...pageProps} />
-      </ChakraProvider>
+      <GovBotProvider>
+        <ChakraProvider theme={theme}>
+          <NavBar />
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </GovBotProvider>
     </SessionProvider>
   )
 }
