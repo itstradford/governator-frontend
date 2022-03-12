e91330cab99c8dfb328e1111babd21ab5ddb7176
kenlok <ken.lok@eezee.com.sg>
2022-03-12 22:51:25 +0800

    remove react context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
-import GovBotProvider from 'components/context-provider/provider'
 import theme from 'config/theme'
 import NavBar from 'components/NavBar'
 
@@ -16,12 +15,10 @@ import '@fontsource/roboto-mono/700.css'
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <GovBotProvider>
         <ChakraProvider theme={theme}>
           <NavBar />
           <Component {...pageProps} />
         </ChakraProvider>
-      </GovBotProvider>
     </SessionProvider>
   )
 }
--- pages/server-select.tsx
+++ pages/server-select.tsx
@@ -13,7 +13,6 @@ import {
   Link,
 } from '@chakra-ui/react'
 
-import { GovBotContext } from '../components/context-provider/provider'
 import Govcrumb from 'components/BreadCrumb'
 
 // const servers = [
@@ -71,82 +70,68 @@ const ServerSelect: NextPage = () => {
   }
 
   return (
-    <GovBotContext.Consumer>
-      {({ state, dispatch }) => {
-        return (
-          <Box bg='dark-2' minH='calc(100vh - 90px)' pt='4rem' pb='8rem'>
-            <Box bg='dark-1' maxW='2xl' mx='auto' p='2rem 3rem'>
-              <Govcrumb />
+    <Box bg='dark-2' minH='calc(100vh - 90px)' pt='4rem' pb='8rem'>
+      <Box bg='dark-1' maxW='2xl' mx='auto' p='2rem 3rem'>
+        <Govcrumb />
 
-              <Flex justifyContent='center' alignItems='center'>
-                {/* Server Select Box */}
+        <Flex justifyContent='center' alignItems='center'>
+          {/* Server Select Box */}
 
-                <Box p={10}>
-                  <VStack spacing={10}>
-                    <Text color='white' fontSize='2xl'>
-                      Select Server
-                    </Text>
+          <Box p={10}>
+            <VStack spacing={10}>
+              <Text color='white' fontSize='2xl'>
+                Select Server
+              </Text>
 
-                    {/* Render server icons */}
-                    <Grid templateColumns='repeat(5, 1fr)' gap={1}>
-                      {servers.map((_server: any, idx) => {
-                        const img = `https://cdn.discordapp.com/icons/${_server.id}/${_server.icon}.png`
+              {/* Render server icons */}
+              <Grid templateColumns='repeat(5, 1fr)' gap={1}>
+                {servers.map((_server: any, idx) => {
+                  const img = `https://cdn.discordapp.com/icons/${_server.id}/${_server.icon}.png`
 
-                        return (
-                          <VStack
-                            key={`server-${idx}`}
-                            cursor='pointer'
-                            role='group'>
-                            <Link href={`/dashboard`} onClick={() => {
-                              console.log('i am clickeddd')
-                              dispatch({ 
-                              type: 'crumb-update', 
-                              crumb: {
-                                name: _server.name,
-                                href: `/dashboard`
-                              } 
-                              })}
-                              }>
-                              <Box
-                                p={1}
-                                borderRadius='full'
-                                _groupHover={{ bg: 'teal' }}>
-                                {_server.icon ? (
-                                  <Image
-                                    src={img}
-                                    alt='user-avatar'
-                                    borderRadius='full'
-                                    boxSize='50px'
-                                  />
-                                ) : (
-                                  <Flex
-                                    borderRadius='full'
-                                    background='grey'
-                                    boxSize='50px'
-                                    justifyContent='center'
-                                    alignItems='center'>
-                                    <Text fontSize='medium'>
-                                      {_server.name.slice(0, 2).toUpperCase()}
-                                    </Text>
-                                  </Flex>
-                                )}
-                              </Box>
-                            </Link>
-                            <Text color='white'>
-                              {_server.name.slice(0, 15)}
-                            </Text>
-                          </VStack>
-                        )
-                      })}
-                    </Grid>
-                  </VStack>
-                </Box>
-              </Flex>
-            </Box>
+                  return (
+                    <VStack key={`server-${idx}`} cursor='pointer' role='group'>
+                      <Link
+                        href={`/dashboard`}
+                        onClick={() => {
+                          console.log('i am clickeddd')
+                          return null  
+                        }
+                        }>
+                        <Box
+                          p={1}
+                          borderRadius='full'
+                          _groupHover={{ bg: 'teal' }}>
+                          {_server.icon ? (
+                            <Image
+                              src={img}
+                              alt='user-avatar'
+                              borderRadius='full'
+                              boxSize='50px'
+                            />
+                          ) : (
+                            <Flex
+                              borderRadius='full'
+                              background='grey'
+                              boxSize='50px'
+                              justifyContent='center'
+                              alignItems='center'>
+                              <Text fontSize='medium'>
+                                {_server.name.slice(0, 2).toUpperCase()}
+                              </Text>
+                            </Flex>
+                          )}
+                        </Box>
+                      </Link>
+                      <Text color='white'>{_server.name.slice(0, 15)}</Text>
+                    </VStack>
+                  )
+                })}
+              </Grid>
+            </VStack>
           </Box>
-        )
-      }}
-    </GovBotContext.Consumer>
+        </Flex>
+      </Box>
+    </Box>
   )
 }
 
